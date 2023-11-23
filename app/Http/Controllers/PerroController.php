4d77a687609ae00d3731a8fdc397613e5f812628
BenjaIGVM <115327548+BenjaIGVM@users.noreply.github.com>
2023-11-23 12:31:44 -0300

    Update PerroController.php

--- app/Http/Controllers/PerroController.php
+++ app/Http/Controllers/PerroController.php
@@ -17,16 +17,33 @@ class PerroController extends Controller
             $perro = new Perro();
             $perro->name = $request->name;
             $perro->url = $request->url;
-            $perro->descripcion = $request->descripcion;
+            $perro->description = $request->descripcion;
             $perro->save();
             return response()->json([
                 "message" => "Perro creado correctamente",
-                "perro" => $perro
-            ], Response::HTTP_OK);
+                "perro" => $perro], Response::HTTP_OK);
         } catch (Exception $e) {
             Log::error($e->getMessage());
             return response()->json([
                 "message" => "Error al crear el perro",
+                "error" => $e->getMessage()
+            ], Response::HTTP_BAD_REQUEST);
+        }
+    }
+    public function viewPerro(PerroRequest $request){
+        try {
+            $perro = Perro::find($request->id);
+            if ($perro) {
+                return response()->json([
+                    "message" => "Perro encontrado correctamente",
+                    "perro" => $perro], Response::HTTP_OK); 
+            }else {
+                throw new Exception("El perro de id: $request->id no se encuentra en la base de datos");
+            }
+        } catch (Exception $e) {
+            Log::error($e->getMessage());
+            return response()->json([
+                "message" => "Error al encontrar perro",
                 "error" => $e->getMessage()
             ], Response::HTTP_BAD_REQUEST);
         }
