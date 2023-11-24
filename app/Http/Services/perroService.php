85b3ce02aac65a5ef2309d493dde56a976b12c84
BenjaIGVM <115327548+BenjaIGVM@users.noreply.github.com>
2023-11-24 12:29:59 -0300

    Update perroService.php

--- app/Http/Services/perroService.php
+++ app/Http/Services/perroService.php
@@ -15,4 +15,23 @@ class PerroService{
         $response = Http::get($url . $id);
         return json_decode($response);
     }
+    public function getRandomDogImage(): string
+    {
+        $maxAttempts = 5; 
+            $attempts = 0;
+
+        do {
+            $url = 'https://dog.ceo/api/breeds/image/random';
+            $response = Http::get($url);
+            $data = json_decode($response->getBody(), true);
+
+            if ($response->successful() && isset($data['message'])) {
+                return $data['message'];
+            }
+
+            $attempts++;
+        } while ($attempts < $maxAttempts);
+
+        return 'https://dog.ceo/api/breeds/image/random'; 
+        }
 }
